ad65657d533a0d3dbaad99875d8dc3dcbad3d033
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-03-29 00:04:11 +0530

    remove unused method

--- conftest.py
+++ conftest.py
@@ -78,45 +78,8 @@ def setup_aws_instance() -> None:
     yield
 
     commands = [
-            f'sudo docker rm --force {container_name}'
-        ]
+        f'sudo docker rm --force {container_name}'
+    ]
     response = AWS.execute_commands(commands=commands)
     print(response)
 
-@pytest.fixture(scope='session', autouse=True)
-def db_connector():
-    pass
-    # import mysql.connector
-    # from mysql.connector import errorcode
-    #
-    # try:
-    #     cred = Common.read_yaml('cred.yaml')
-    #     mysql_src = cred['mysql_src']
-    #     mysql_dst = cred['mysql_dst']
-    #
-    #     _db_connection_src = mysql.connector.connect(**mysql_src)
-    #     db_cursor_src = _db_connection_src.cursor()
-    #
-    #     _db_connection_dest = mysql.connector.connect(**mysql_dst)
-    #     db_cursor_dest = _db_connection_dest.cursor()
-    #
-    # except mysql.connector.Error as err:
-    #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    #         print("Something is wrong with your user name or password")
-    #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    #         print("Database does not exist")
-    #     else:
-    #         print(err)
-    #
-    # print("connection established")
-    #
-    # yield {'src': {'connection': _db_connection_src, 'cursor': db_cursor_src},
-    #        'dst': {'connection': _db_connection_dest, 'cursor': db_cursor_dest}}
-    #
-    # # close db cursors
-    # db_cursor_src.close()
-    # db_cursor_dest.close()
-    #
-    # # close db connection
-    # _db_connection_src.close()
-    # _db_connection_dest.close()
